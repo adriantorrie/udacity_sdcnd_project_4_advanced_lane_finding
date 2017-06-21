0fcfb2d6e04cd0507222331fbdb576e5aa6b3d68
Adrian Torrie <adriantorrie@gmail.com>
2017-06-22 01:24:01 +1000

    combined thresholds

--- src/pipeline.py
+++ src/pipeline.py
@@ -19,109 +19,17 @@ def undistort_img(img, M, dist):
     return cv2.undistort(img, M, dist, None, M)
 
 
-def abs_sobel_thresh(img, orient='x', thresh_min=50, thresh_max=100):
+def binary_threshold(img, thresh):
+    binary_output = np.zeros_like(img)
+    binary_output[(img > thresh[0]) & (img <= thresh[1])] = 1
+    return binary_output
+
+
+def abs_sobel_thresh(sobel, thresh=(20, 100)):
     """Return absolute sobel threshold image for further processing
 
-    Steps taken
-        1) Convert to grayscale
-        2) Take the derivative in x or y given orient = 'x' or 'y'
-        3) Take the absolute value of the derivative or gradient
-        4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
-        5) Create a mask of 1's where the scaled gradient magnitude
-                is > thresh_min and < thresh_max
-
     Args:
-        img (image): the image to apply the transform to
-
-        orient (str): One of ['x', 'y'] axis to apply sobel operator to
-
-        thresh_min (int): lower bound for determining binary output
-
-        thresh_max (int`):: upper bound for determining binary output
-
-    Returns:
-        image: binary threshold image
-    """
-
-    # Define a function that takes an image, gradient orientation,
-    # and threshold min / max values.
-
-    # 1
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    # 2 & 3
-    if orient == 'x':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
-    if orient == 'y':
-        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
-
-    # 4
-    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
-
-    # 5
-    binary_output = np.zeros_like(scaled_sobel)
-    binary_output[
-        (scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
-
-    return binary_output
-
-
-def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-    """Apply a threshold to the overall magnitude of the gradient
-
-    Steps taken
-        1) Convert to grayscale
-        2) Take the gradient in x and y separately
-        3) Calculate the magnitude
-        4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
-        5) Create a binary mask where mag thresholds are met
-
-    Args:
-        img (image): the image to apply the transform to
-
-        sobel_kernel (int): (default=3) region to apply magnitude over
-
-        mag_thresh (tuple): (lower_bound, upper_bound)
-                            lower_bound = lower bound for determining output
-                            upper_bound = upper bound for determining output
-
-    Returns:
-        image: binary threshold image
-    """
-    # 1
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    # 2
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-
-    # 3
-    gradmag = np.sqrt(sobelx ** 2 + sobely ** 2)
-
-    # 4
-    scale_factor = np.max(gradmag) / 255
-    gradmag = (gradmag / scale_factor).astype(np.uint8)
-
-    # 5
-    binary_output = np.zeros_like(gradmag)
-    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
-
-    return binary_output
-
-
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
-    """
-    Apply the following steps to img
-        1) Convert to grayscale
-        2) Take the gradient in x and y separately
-        3) Take the absolute value of the x and y gradients
-        4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
-        5) Create a binary mask where direction thresholds are met
-
-    Args:
-        img (image): the image to apply the transform to
-
-        sobel_kernel (int): (default=3) region to apply magnitude over
+        sobel (image): the sobel image to apply the transform to
 
         thresh (tuple): (lower_bound, upper_bound)
                         lower_bound = lower bound for determining output
@@ -130,26 +38,75 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
     Returns:
         image: binary threshold image
     """
-    # 1
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-    # 2
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-
-    # 3 & 4
-    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-
-    # 5
-    binary_output = np.zeros_like(absgraddir)
-    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
-
-    return binary_output
+    sobel = np.absolute(sobel)
+    sobel = np.uint8(255 * sobel / np.max(sobel))
+    return binary_threshold(sobel, thresh)
 
 
-def binary_threshold():
+# def mag_thresh(x_sobel, y_sobel, thresh=(0, 255)):
+#     """Apply a threshold to the overall magnitude of the gradient
+#
+#     Args:
+#         x_sobel (image): sobel image of gradients across the x axis
+#
+#         y_sobel (image): sobel image of gradients across the y axis
+#
+#         thresh (tuple): (lower_bound, upper_bound)
+#                         lower_bound = lower bound for determining output
+#                         upper_bound = upper bound for determining output
+#
+#     Returns:
+#         image: binary threshold image
+#     """
+#     sobel = np.sqrt(x_sobel ** 2 + y_sobel ** 2)
+#     scale_factor = np.max(sobel) / 255
+#     sobel = (sobel / scale_factor).astype(np.uint8)
+#     return binary_threshold(sobel, thresh)
 
-    return binary_output
+
+# def dir_threshold(x_sobel, y_sobel, thresh=(0, np.pi / 4)):
+#     """Apply direction threshold
+#
+#     Args:
+#         x_sobel (image): sobel image of gradients across the x axis
+#
+#         y_sobel (image): sobel image of gradients across the y axis
+#
+#         thresh (tuple): (lower_bound, upper_bound)
+#                         lower_bound = lower bound for determining output
+#                         upper_bound = upper bound for determining output
+#
+#     Returns:
+#         image: binary threshold image
+#     """
+#     sobel = np.arctan2(np.absolute(y_sobel), np.absolute(x_sobel))
+#     return binary_threshold(sobel, thresh)
+
+
+def combined_binary(img, ksize=3):
+    """Apply gradien and colour binary threshold transform"""
+    # ---------------------------------------------------------
+    # Colour binary
+    hls = cv2.cvtColor(np.copy(img), cv2.COLOR_RGB2HLS)
+    r_binary = binary_threshold(img[:, :, 0], (225, 255))
+    s_binary = binary_threshold(hls[:, :, 2], (170, 255))
+    colour_binary = np.zeros_like(r_binary)
+    colour_binary[((r_binary == 1) |
+                   (s_binary == 1))] = 1
+
+    # ---------------------------------------------------------
+    # Sobel binary
+    channel = hls[:, :, 2]
+    x_sobel = cv2.Sobel(channel, cv2.CV_64F, 1, 0, ksize=ksize)
+    sobel_binary = abs_sobel_thresh(x_sobel, thresh=(20, 100))
+
+    # ---------------------------------------------------------
+    # Combined binary
+    combined_binary = np.zeros_like(r)
+    combined_binary[((colour_binary == 1) |
+                     (sobel_binary == 1))] = 1
+
+    return combined_binary
 
 
 def get_perspective_matrices():
@@ -159,27 +116,26 @@ def get_perspective_matrices():
     src = np.float32([src_list])
     dst = np.float32([dst_list])
 
-    M = cv2.getPerspectiveTransform(src, dst)
-    M_inv = cv2.getPerspectiveTransform(dst, src)
+    pers = cv2.getPerspectiveTransform(src, dst)
+    pers_inv = cv2.getPerspectiveTransform(dst, src)
 
-    return M, M_inv
+    return pers, pers_inv
 
 
 def warper(img, M):
     img_size = (img.shape[1], img.shape[0])
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-
     return warped
 
 
-def run_pipeline(img):
+def run_pipeline(img, M, dist):
     # locals
-    M, M_inv = get_perspective_matrices()
+    pers, pers_inv = get_perspective_matrices()
 
     # pipeline
-    img = undistort_img(img)
-    img = binary_threshold(img)
-    img = warper(img, M)
+    img = undistort_img(img, M, dist)
+    img = combined_binary(img)
+    img = warper(img, pers)
 
     return img
 
